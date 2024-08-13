a83b939f8a3a163f4affee7a849217f66b03d3aa
Louis Nguyen (ext.) <Louis.Nguyen@senacor.com>
2024-08-13 15:07:19 +0200

    11.12: Throwing error

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 8080
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
